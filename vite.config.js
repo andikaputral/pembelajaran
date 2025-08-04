db3fb1e5645a1549f15602cadba7286eac21d6a9
andikaputral <andikaputralikha@gmail.com>
2025-08-04 14:10:12 +0700

    update package and vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/pembelajaran/",
+	base: "/pembelajaran",
 	plugins: [react(), tailwindcss()],
 });
