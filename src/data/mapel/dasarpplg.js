ab306b69df00c8a782343f5c2f4aac46baf71bb6
andikaputral <andikaputralikha@gmail.com>
2025-08-09 11:39:19 +0700

    image update

--- src/data/mapel/dasarpplg.js
+++ src/data/mapel/dasarpplg.js
@@ -5,8 +5,7 @@ export const class1 = {
 	title: "Dasar-Dasar Pemrograman Perangkat Lunak dan Gim",
 	description:
 		"Memahami tentang teknik pemrograman, merancang perangkat lunak, basis data, dan berbagai jenis perangkat lunak dan gim.",
-	image:
-		"https://images.unsplash.com/photo-1550439062-609e1531270e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	image: "/pembelajaran/img/max-duzij-qAjJk-un3BI-unsplash.jpg",
 	disabled: false,
 	modules: [module1],
 };
--- src/data/mapel/webdev.js
+++ src/data/mapel/webdev.js
@@ -8,8 +8,7 @@ export const class2 = {
 	title: "Web Development",
 	description:
 		"Memahami secara dasar untuk membangun sebuah website dengan HTML, CSS, dan Javascript.",
-	image:
-		"https://images.unsplash.com/photo-1733412505442-36cfa59a4240?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	image: "/pembelajaran/img/mohammad-rahmani-oXlXu2qukGE-unsplash.jpg",
 	disabled: false,
 	modules: [module1, module2, module3, module4],
 };
